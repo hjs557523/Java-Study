896bb949f9eaf4115d1a89ce03183cf33502b0ad
hjs557523 <326464623@qq.com>
2019-07-17 23:34:46 +0800

    2019/7/17

---
+++ 代理模式/Cglib代理/App.java
@@ -0,0 +1,17 @@
+/**
+ * 测试类
+ */
+public class App {
+
+    @Test
+    public void test(){
+        //目标对象
+        UserDao target = new UserDao();
+
+        //代理对象
+        UserDao proxy = (UserDao)new ProxyFactory(target).getProxyInstance();
+
+        //执行代理对象的方法
+        proxy.save();
+    }
+}
---
+++ 代理模式/Cglib代理/UserDao.java
@@ -0,0 +1,8 @@
+/**
+ * 目标对象，没有实现任何接口
+ */
+public class UserDao {
+    public void save(){
+        System.out.println("保存数据成功！");
+    }
+}
