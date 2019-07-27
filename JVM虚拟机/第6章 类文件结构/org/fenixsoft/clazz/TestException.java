9b7995321101580d5c7c40c240489a71e5d127ac
hjs557523 <326464623@qq.com>
2019-07-28 00:40:14 +0800

    2019/7/28

---
+++ JVM虚拟机/第6章 类文件结构/org/fenixsoft/clazz/TestException.java
@@ -0,0 +1,15 @@
+package org.fenixsoft.clazz;
+public class TestException {
+    public int inc() {
+        int x;
+        try {
+            x = 1;
+            return x;
+        } catch (Exception e) {
+            x = 2;
+            return x;
+        } finally {
+            x = 3;
+        }
+    }
+}
