7fa35a06599942d68f0f41fe8686241501fc8686
hjs557523 <326464623@qq.com>
2019-08-07 09:47:21 +0800

    2019/8/7

---
+++ JVM虚拟机/第8章 虚拟机字节码执行引擎/org/hjs/byteCodeRun/StaticResolution.java
@@ -0,0 +1,13 @@
+/**
+ * 方法静态解析演示
+ */
+
+public class StaticResolution {
+    public static void sayHello() {
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        StaticResolution.sayHello();
+    }
+}
